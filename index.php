1539113ee724f34417bab18c8084f3160f0dfce0
abdulkerim <abdulkerimademrcdf@gmail.com>
2024-05-26 21:41:04 -0700

    nav bar and hero section

--- index.php
+++ index.php
@@ -3,13 +3,56 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Your flight</title>
 </head>
 
 <body>
-    <!-- -->
+    <!-- nav bar -->
+    <nav>
+        <div id="part1">
+            <img src="" alt="logo">
+            <a href="">home</a>
+            <a href="">about us</a>
+            <a href="">help</a>
+        </div>
 
+        <div id="part2">
+            <div id="country">
+                <img src="" alt="flg">
+            </div>
+
+            <div class="signin">
+                <i class="fa fa-user-o" aria-hidden="true"></i>
+                <span>login | signup</span>
+            </div>
+        </div>
+    </nav>
+
+    <!-- main sectiion of this page-->
+    <main>
+        <section id="sec1">
+            <!-- hero section -->
+            <div id="hero">
+                Book Your Perfect flight <br>
+                with x
+            </div>
+            <p>
+                get your ticket eisly, avoid the hustle of geting tickets
+                instade be rady for your flight.
+            </p>
+
+            <a href="#">
+                <button>discover places</button>
+            </a>
+        </section>
+
+        <section id="sec2">
+
+        </section>
+
+
+    </main>
 </body>
 
 </html>
